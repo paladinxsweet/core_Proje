54ace12d4a23435061cbcf8ff618278ae1c53dde
IbrahimHalilMetin <94048854+paladinxsweet@users.noreply.github.com>
2023-11-16 14:59:33 +0300

    Proje dosyası ekle.

---
+++ DataAccessLayer/Migrations/20231028170619_mig_addcolumn_contact.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_addcolumn_contact : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Github",
+                table: "Contacts",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Github",
+                table: "Contacts");
+        }
+    }
+}
---
+++ core_Proje/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿ using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace core_Proje.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "İstatistikler";
+            ViewBag.v2 = "İstatik Sayfası";
+            ViewBag.v3 = "İstatistikler";
+            return View();
+        }
+    }
+}
